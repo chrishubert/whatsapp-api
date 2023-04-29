e588e1e353c713427546a4b4d963b4e6eb205a13
Christophe Hubert <hubert.christophe@gmail.com>
2023-04-29 18:07:12 +0700

    Improve env management

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const globalApiKey = process.env.API_KEY
 const baseWebhookURL = process.env.BASE_WEBHOOK_URL
 const maxAttachmentSize = process.env.MAX_ATTACHMENT_SIZE || 10000000
 const setMessagesAsSeen = process.env.SET_MESSAGES_AS_SEEN === 'TRUE'
-const disabledCallbacks = process.env.DISABLED_CALLBACKS.split('|')
+const disabledCallbacks = process.env.DISABLED_CALLBACKS ? process.env.DISABLED_CALLBACKS.split('|') : []
 
 module.exports = {
   sessionFolderPath,
