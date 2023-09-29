86af1cfcf1eaff4f749e035b5c1864e4bd619e48
Anton Voylenko <anton.voylenko@gmail.com>
2023-09-29 11:38:05 +0300

    rollback headless mode

--- src/sessions.js
+++ src/sessions.js
@@ -90,7 +90,7 @@ const setupSession = (sessionId) => {
     const clientOptions = {
       puppeteer: {
         executablePath: process.env.CHROME_BIN || null,
-        headless: false,
+        // headless: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
       },
       userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
