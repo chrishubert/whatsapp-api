4db2abd22a332118708a99aee8b002399a3cc466
rodolphoguerreiro <rodolpho_warrior@hotmail.com>
2024-05-01 18:00:53 -0300

    refactor: :zap:

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 // setup global const
 const sessionFolderPath = process.env.SESSIONS_PATH || './sessions'
 const enableLocalCallbackExample = (process.env.ENABLE_LOCAL_CALLBACK_EXAMPLE || '').toLowerCase() === 'true'
-const globalApiKey = process.env.API_KEY
+const globalApiKey = process.env.API_WHATSAPP_KEY
 const baseWebhookURL = process.env.BASE_WEBHOOK_URL
 const maxAttachmentSize = parseInt(process.env.MAX_ATTACHMENT_SIZE) || 10000000
 const setMessagesAsSeen = (process.env.SET_MESSAGES_AS_SEEN || '').toLowerCase() === 'true'
