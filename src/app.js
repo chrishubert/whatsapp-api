cb36db8fff4a6e69a5a5f1657644b03932b5298c
Justany ITOUA <justany2014@gmail.com>
2024-05-05 17:11:47 +0100

    Pr: update config.. files..

--- src/app.js
+++ src/app.js
@@ -1,35 +1,18 @@
-require('./routes')
-const { restoreSessions } = require('./sessions')
-const { routes } = require('./routes')
-const app = require('express')()
-const bodyParser = require('body-parser')
-const { maxAttachmentSize } = require('./config')
+require("./routes");
+const { restoreSessions } = require("./sessions");
+const { routes } = require("./routes");
+const app = require("express")();
+const bodyParser = require("body-parser");
+const { maxAttachmentSize } = require("./config");
 
 // Initialize Express app
-app.disable('x-powered-by')
-app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }))
-app.use(bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true }))
-app.use('/', routes)
+app.disable("x-powered-by");
+app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }));
+app.use(
+  bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true })
+);
+app.use("/", routes);
 
-restoreSessions()
+restoreSessions();
 
-module.exports = app
-
-
-// const { createServer } = require("http");
-// const { restoreSessions } = require("./sessions");
-// const { routes } = require("./routes");
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const { maxAttachmentSize } = require("./config");
-
-// // Initialiser l'application Express
-// app.disable("x-powered-by");
-// app.use(bodyParser.json({ limit: maxAttachmentSize + 1000000 }));
-// app.use(
-//   bodyParser.urlencoded({ limit: maxAttachmentSize + 1000000, extended: true })
-// );
-// app.use("/", routes);
-
-// restoreSessions();
-// module.exports = app;
+module.exports = app;
